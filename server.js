a99adc7814db226b2ca2c6027d4b4626f4abc6af
314rce <piercebyers@aol.com>
2016-10-22 20:36:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-// var mongo = require('mongodb').MongoClient;
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io=require('socket.io').listen(server);
     server.listen(8000);
-// var myCollection;
-// var db = mongo.connect('mongodb://127.0.0.1:27017/chat', function(err, db) {
-//     if(err)
-//         throw err;
-//     console.log("connected to the mongoDB !");
-//     myCollection = db.collection('messages');
-//   }
-// );
 
 app.use('/client/',express.static('./client/'));
 app.use('/socketio/',express.static(__dirname + '/node_modules/socket.io/'))
@@ -38,13 +29,6 @@ var storage = createStorage();
 app.get('/',function(req,res){
   res.sendfile('./client/index.html')
 });
-
-//update chat messages
-// app.post('/',function(res){
-//   var messages = db.messages.find().forEach(span(this.name,this.message));
-//   res.sendStatus(201);
-//   res.json(messages);
-// });
 
 io.sockets.on('connection',function(socket){
   io.sockets.emit('stored-msgs', storage.messages);
